9e9b582b12a8820a2782de3920461ea80a563edb
Dark-Existed <c2572388311@gmail.com>
2017-05-09 21:58:06 +0800

    增加使用命令行以及文件输出

--- coursework/main.cpp
+++ coursework/main.cpp
@@ -1,18 +1,31 @@
 #include <iostream>
 #include <time.h>
 #include <string>
+#include <fstream>
 #include "CalEquation.h"
 #include "Generated.h"
 #include "ProcessLanguage.h"
 using namespace std;
 
-int main()
+int main(int argc, char const *argv[])
 {
 	srand(unsigned(time(0)));
-	int count, t=0, f ,temp;
+	
+    int count=0, t=0, f, temp, flag=0;
 	float input, answer;
-	string equation , location,clanguage;
+	string equation, location, clanguage;
 	char LanguageRead[20][200],end;
+
+    ifstream fin;
+    fin.open(argv[1]);
+	fin >> count;
+	if (count != 0)
+		flag = 1;
+	fin.close();
+
+    ofstream fout;
+    fout.open(argv[2]);
+
 	cout<<"Enter the serial number to select the language."<<endl;
     cout<<"==================================================================="<<endl;
     LanguageList();
@@ -26,7 +39,13 @@ int main()
     dispose(LanguageRead,&location);
     cout<<LanguageRead[0]<<endl;
     cout<<LanguageRead[1];
-    cin>>count;
+    fout<<LanguageRead[1]<<count<<endl;
+    
+
+
+    if (flag != 1)
+        cin>>count;
+    
     for (int i = 0; i < count; ++i)
     {
     	equation = generateExpression();
@@ -34,15 +53,19 @@ int main()
     	temp=(int)(answer*100+0.5f);//四舍五入保留两位小数
         answer=(float)temp/100;
 		cout << '<' << i + 1 << "> " << equation << '=';
+		fout << '<' << i + 1 << "> " << equation << '=' << answer << endl;
     	cin>>input;
     	if (input==answer)
     	{
     		t++;
     		cout<<LanguageRead[2]<<endl;
+            fout<<LanguageRead[2]<<endl;
+
     	}
     	else
-    		cout<<LanguageRead[3]<<answer<<endl;
-
+    	{	cout<<LanguageRead[3]<<answer<<endl;
+            fout<<LanguageRead[3]<<answer<<endl;
+        }
     }
     f = count-t;
 	cout << LanguageRead[4] << endl;
@@ -50,7 +73,12 @@ int main()
 	if (end == 'Q' || end == 'q')
 	{
 		cout << LanguageRead[5] << t << LanguageRead[7] << endl;
-		cout << LanguageRead[6] << f << LanguageRead[7];
+        fout << LanguageRead[5] << t << LanguageRead[7] << endl;
+		
+        cout << LanguageRead[6] << f << LanguageRead[7];
+        fout << LanguageRead[6] << f << LanguageRead[7];
 	}
+
+    fout.close();
 	return 0;
 }
